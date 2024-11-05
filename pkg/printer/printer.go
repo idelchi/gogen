3da4d223a3705d562cb657a97b4fbcc3acc5893e
idelchi <64003206+idelchi@users.noreply.github.com>
2024-11-05 10:40:42 +0100

    dev

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -1,4 +1,6 @@
 // Package printer provides a simple way to print messages to the standard output and standard error streams.
+//
+//nolint:forbidigo
 package printer
 
 import (
@@ -8,20 +10,20 @@ import (
 
 // Stdoutln prints a message to the standard output stream, appending a newline.
 func Stdoutln(format string, args ...any) {
-	fmt.Println(fmt.Sprintf(format, args...))
+	Stdout(format+"\n", args...)
 }
 
 // Stderrln prints a message to the standard error stream, appending a newline.
 func Stderrln(format string, args ...any) {
-	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, args...))
+	Stderr(format+"\n", args...)
 }
 
 // Stdout prints a message to the standard output stream.
 func Stdout(format string, args ...any) {
-	fmt.Print(fmt.Sprintf(format, args...))
+	fmt.Printf(format, args...)
 }
 
 // Stderr prints a message to the standard error stream.
 func Stderr(format string, args ...any) {
-	fmt.Fprint(os.Stderr, fmt.Sprintf(format, args...))
+	fmt.Fprintf(os.Stderr, format, args...)
 }
