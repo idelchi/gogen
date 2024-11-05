2ab00ca5e00b4098af74f70eee08b4f222aaaf2d
idelchi <64003206+idelchi@users.noreply.github.com>
2024-11-05 12:14:59 +0100

    dev

--- internal/commands/hash.go
+++ internal/commands/hash.go
@@ -8,8 +8,6 @@ import (
 	"github.com/idelchi/gogen/internal/config"
 	"github.com/idelchi/gogen/pkg/cobraext"
 	"github.com/idelchi/gogen/pkg/hash"
-	"github.com/idelchi/gogen/pkg/printer"
-	"github.com/idelchi/gogen/pkg/stdin"
 )
 
 // NewHashCommand creates the hash subcommand for password hashing operations.
@@ -21,23 +19,12 @@ func NewHashCommand(cfg *config.Config) *cobra.Command {
 		Long:  "Hash a password using bcrypt with configurable cost and benchmarking.",
 		Args:  cobra.MaximumNArgs(1),
 		PreRunE: func(_ *cobra.Command, args []string) error {
-			isPiped := stdin.IsPiped()
-
-			switch {
-			case len(args) > 0:
-				// Prioritize argument if it exists, regardless of stdin
-				cfg.Password.Password = args[0]
-				if isPiped {
-					printer.Stderrln("reading password from argument, ignoring stdin")
-				}
-			case isPiped:
-				// No arg but stdin is piped
-				password, err := stdin.Read()
-				if err != nil {
-					return fmt.Errorf("reading password from stdin: %w", err)
-				}
-				cfg.Password.Password = password
+			arg, err := cobraext.PipeOrArg(args)
+			if err != nil {
+				return fmt.Errorf("reading password: %w", err)
 			}
+
+			cfg.Password.Password = arg
 
 			return cobraext.Validate(cfg, &cfg.Password)
 		},
